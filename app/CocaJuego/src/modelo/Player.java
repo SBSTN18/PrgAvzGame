cf3f8416df64fee2f74fb0b18624c0597cca8ce2
SBSTN18 <sebastian.zambranoh18@gmail.com>
2026-02-27 19:52:37 -0500

    segunda version del otro modelo

--- app/CocaJuego/src/modelo/Player.java
+++ app/CocaJuego/src/modelo/Player.java
@@ -1,19 +1,14 @@
+package modelo;
+
 public class Player {
 
     private String name;
     private int code;
-    private int points;
-    private int attemps;
-    private int cantEmbo;
-    private boolean status;
 
     public Player(String name, int code) {
         this.name = name;
         this.code = code;
-        this.points = 0;
-        this.attemps = 0;
-        this.cantEmbo = 0;
-        this.status = false;
+        ;
     }
 
     public String getName() {
@@ -23,38 +18,5 @@ public class Player {
     public int getCode() {
         return code;
     }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public int getAttemps() {
-        return attemps;
-    }
-
-    public int getCantEmbo() {
-        return cantEmbo;
-    }
-
-    public boolean getStatus() {
-        return status;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public void setAttemps(int attemps) {
-        this.attemps = attemps;
-    }
-
-    public void setCantEmbo(int cantEmbo) {
-        this.cantEmbo = cantEmbo;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }   
-
 
 }
--- app/CocaJuego/src/modelo/Team.java
+++ app/CocaJuego/src/modelo/Team.java
@@ -1,3 +1,5 @@
+package modelo;
+
 import java.util.List;
 
 
@@ -7,20 +9,14 @@ public class Team {
     private String name;
     private List<Player> players;
     private int pin;
-    private int score;
-    private int totalAttemps;
-    private int totalEmbo;
-    private boolean ready;
+    
 
     public Team(String proyect, String name, List<Player> players, int pin) {
         this.proyect = proyect;
         this.name = name;
         this.players = players;
         this.pin = pin;
-        this.score = 0;
-        this.totalAttemps = 0;
-        this.totalEmbo = 0;
-        this.ready = false;
+        
     }
 
     public String getProyect() {
@@ -39,40 +35,10 @@ public class Team {
         return pin;
     }
 
-    public int getScore() {
-        return score;
-    }   
-
-    public int getTotalAttemps() {
-        return totalAttemps;
-    }           
-
-    public int getTotalEmbo() {
-        return totalEmbo;
-    }
-
-    public boolean getReady() {
-        return ready;
-    }
+   
 
     public int getCantPlayers() {
         return players.size();
-    }
-
-    public void calculateScore(int points) {
-        this.score += points;
-    }
-
-    public void calculateTotalAttemps(int attemps) {
-        this.totalAttemps += attemps;
-    }
-
-    public void calculateTotalEmbo(int embo) {
-        this.totalEmbo += embo;
-    }
-
-    public void setReady(boolean ready) {
-        this.ready = ready;
     }
 
 }
